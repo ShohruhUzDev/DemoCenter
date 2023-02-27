735d7938007d7247e055ecfd8e3dabcabc6029bb
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-27 17:08:12 +0500

    DATA : Create Class & Migrate User

---
+++ DemoCenter/Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,10 @@
+﻿using DemoCenter.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoCenter.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DemoCenter/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,25 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DemoCenter.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+            
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
