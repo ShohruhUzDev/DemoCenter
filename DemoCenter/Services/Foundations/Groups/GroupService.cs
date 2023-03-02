647c1636b21fe00ae137d79cbe39a781c24daf9b
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-02 20:21:27 +0500

    ShouldAddGroupAsync ->FAIL

---
+++ DemoCenter/Services/Foundations/Groups/GroupService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DemoCenter.Brokers.Storages;
+using DemoCenter.Models.Groups;
+
+namespace DemoCenter.Services.Foundations.Groups
+{
+    public class GroupService : IGroupService
+    {
+        private IStorageBroker storageBroker;
+
+        public GroupService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Group> AddGroupAsync(Group group) =>
+            throw new System.NotImplementedException();
+        
+    }
+}
