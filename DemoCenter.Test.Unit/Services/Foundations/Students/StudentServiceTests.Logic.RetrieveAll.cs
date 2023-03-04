122411688d0f9f671e9ee5ef9e649c74557df3c5
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 02:04:19 +0500

    FOUNDATIONS : RetrieveAll Students

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using DemoCenter.Models.Students;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public void ShoulRetrieveAllStudents()
+        {
+            //given
+            IQueryable<Student> randomStudents = CreateRandomStudents();
+            IQueryable<Student> storageStudents = randomStudents;
+            IQueryable<Student> expectedStudents = storageStudents;
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectAllStudents()).Returns(storageStudents);
+
+            //when
+            IQueryable<Student> actualStudents =
+                this.studentService.RetrieveAllStudents();
+
+            //then
+            actualStudents.Should().BeEquivalentTo(expectedStudents);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.SelectAllStudents(), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
