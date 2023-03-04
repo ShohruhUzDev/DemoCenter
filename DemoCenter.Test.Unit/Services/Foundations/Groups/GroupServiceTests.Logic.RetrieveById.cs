78352a0190a9dc876b8201241197eb5106a12273
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 02:02:16 +0500

    FOUNDATIONS : RetrieveById Group

---
+++ DemoCenter.Test.Unit/Services/Foundations/Groups/GroupServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Groups
+{
+    public partial class GroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveByIdGroupAsync()
+        {
+            //given
+            Guid randomGroupId = Guid.NewGuid();
+            Guid inputGroupId = randomGroupId;
+            Group randomGroup = CreateRandomGroup();
+            Group storedGroup = randomGroup;
+            Group expectedGroup = storedGroup.DeepClone();
+
+            this.storageBrokerMock.Setup(broker=>
+            broker.SelectGroupByIdAsync(inputGroupId)).ReturnsAsync(storedGroup);
+
+            //when
+           Group actualGroup= await this.groupService.RetrieveGroupByIdAsync(inputGroupId);
+
+            //then
+            actualGroup.Should().BeEquivalentTo(expectedGroup);
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.SelectGroupByIdAsync(inputGroupId), Times.Once());   
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();    
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();   
+        }
+    }
+}
