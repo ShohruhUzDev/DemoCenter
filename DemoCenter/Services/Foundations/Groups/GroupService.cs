13c730ea39d63d367daef45e95641f2716c3e60b
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 01:57:46 +0500

    FOUNDATIONS :Retrieve And RetrieveById

--- DemoCenter/Services/Foundations/Groups/GroupService.cs
+++ DemoCenter/Services/Foundations/Groups/GroupService.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoCenter.Brokers.DateTimes;
+using DemoCenter.Brokers.Loggings;
 using DemoCenter.Brokers.Storages;
 using DemoCenter.Models.Groups;
 
@@ -7,12 +11,28 @@ namespace DemoCenter.Services.Foundations.Groups
     public class GroupService : IGroupService
     {
         private IStorageBroker storageBroker;
+        private IDateTimeBroker dateTimeBroker;
+        private ILoggingBroker loggingBroker;
 
-        public GroupService(IStorageBroker storageBroker) =>
+        public GroupService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
             this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
 
+        }
         public async ValueTask<Group> AddGroupAsync(Group group) =>
             await this.storageBroker.InsertGroupAsync(group);
 
+        public IQueryable<Group> RetrieveAllGroups() =>
+            this.storageBroker.SelectAllGroups();
+        
+
+        public ValueTask<Group> RetrieveGroupByIdAsync(Guid groupId) =>
+            this.storageBroker.SelectGroupByIdAsync(groupId);
+        
     }
 }
