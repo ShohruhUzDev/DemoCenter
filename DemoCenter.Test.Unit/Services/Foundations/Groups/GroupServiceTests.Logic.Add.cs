3bd3e750b45d318f8f88af0dabef031f573cd206
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 02:01:33 +0500

    FOUNDATIONS : Add Group

---
+++ DemoCenter.Test.Unit/Services/Foundations/Groups/GroupServiceTests.Logic.Add.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Groups
+{
+    public partial class GroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddGroupAsync()
+        {
+            //given
+            Group randomGroup = CreateRandomGroup();
+            Group inputGroup = randomGroup;
+            Group persistedGroup = inputGroup;
+            Group expectedGroup = persistedGroup.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertGroupAsync(inputGroup)).ReturnsAsync(persistedGroup);
+
+            //when
+            Group actualGroup = await this.groupService
+                .AddGroupAsync(inputGroup);
+
+            //then
+            actualGroup.Should().BeEquivalentTo(expectedGroup);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertGroupAsync(inputGroup), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
