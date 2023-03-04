5a7d7bba28f810c06d5871ec92cd14689318f03b
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 02:08:04 +0500

    FOUNDATIONS : RetrieveAll Teachers

---
+++ DemoCenter.Test.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Teachers;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Teachers
+{
+    public partial class TeacherServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllTeachers()
+        {
+            //given
+            IQueryable<Teacher> randomTeachers = CreateRandomTeachers();
+            IQueryable<Teacher> storageTeachers = randomTeachers;
+            IQueryable<Teacher> expectedTeachers= storageTeachers;
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectAllTeachers()).Returns(storageTeachers);
+
+            //when
+            IQueryable<Teacher> actualTeachers =
+                this.teacherService.RetrieveAllTeachers();
+
+            //then
+            actualTeachers.Should().BeEquivalentTo(expectedTeachers);   
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.SelectAllTeachers(), Times.Once());
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
