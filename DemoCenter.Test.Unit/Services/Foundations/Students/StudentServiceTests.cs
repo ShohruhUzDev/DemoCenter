397cebb86dae82d484224a00bcdc4b1107039934
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 02:03:12 +0500

    FOUNDATIONS : Add UnitTest

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using DemoCenter.Brokers.DateTimes;
+using DemoCenter.Brokers.Loggings;
+using DemoCenter.Brokers.Storages;
+using DemoCenter.Models.Students;
+using DemoCenter.Services.Foundations.Students;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IStudentService studentService;
+        public StudentServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.studentService = new StudentService(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+
+        private DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private IQueryable<Student> CreateRandomStudents()
+        {
+            return CreateStudentFiller(dates: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber()).AsQueryable();
+        }
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 99).GetValue();
+        private Student CreateRandomStudent() =>
+            CreateStudentFiller(dates: GetRandomDateTimeOffset()).Create();
+
+        private Filler<Student> CreateStudentFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Student>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
