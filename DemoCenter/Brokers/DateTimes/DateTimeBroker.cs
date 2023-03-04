edeb6030d2a96118069203ce96bbce43e833aa09
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-05 01:56:46 +0500

    CONFIG :Add DateTimeBroker

---
+++ DemoCenter/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DemoCenter.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTime GetCurrenDateTime() =>
+            DateTime.UtcNow;
+    }
+}
