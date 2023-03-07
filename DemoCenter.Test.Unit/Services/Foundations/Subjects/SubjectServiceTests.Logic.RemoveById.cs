588001d8c15a4204beca7558968931bab1ece03f
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:09:41 +0500

    FOUDATIONS : Logic RemoveByIdSubject Test

---
+++ DemoCenter.Test.Unit/Services/Foundations/Subjects/SubjectServiceTests.Logic.RemoveById.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Subjects;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Subjects
+{
+    public partial class SubjectServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveSubjectByIdAsync()
+        {
+            //given
+            Guid randomSubjectId= Guid.NewGuid();
+            Guid inputSubjectId = randomSubjectId;
+            Subject randomSubject=CreateRandomSubject();
+            Subject storageSubject = randomSubject;
+            Subject expectedInputSubject=storageSubject;
+            Subject deletedSubject = expectedInputSubject;
+            Subject expectedSubject = deletedSubject.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectSubjectByIdAsync(inputSubjectId)).ReturnsAsync(storageSubject);
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.DeleteSubjectAsync(expectedInputSubject)).ReturnsAsync(deletedSubject);  
+
+            //when
+            Subject actualSubject=await 
+                this.subjectService.RemoveSubjectByIdAsync(inputSubjectId);
+
+            //then
+            actualSubject.Should().BeEquivalentTo(expectedSubject);
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.SelectSubjectByIdAsync(inputSubjectId), Times.Once());
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.DeleteSubjectAsync(expectedInputSubject), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
