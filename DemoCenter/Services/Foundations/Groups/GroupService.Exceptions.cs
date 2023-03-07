dbe16133149527750dec2062f11c409b1ac77d27
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:05:56 +0500

    FOUDATIONS : Add Exceptions And Validations Classes

---
+++ DemoCenter/Services/Foundations/Groups/GroupService.Exceptions.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using DemoCenter.Models.Groups.Exceptions;
+using Xeptions;
+
+namespace DemoCenter.Services.Foundations.Groups
+{
+    public partial class GroupService
+    {
+        private  delegate ValueTask<Group> ReturningGroupFunction();
+
+        private async ValueTask<Group> TryCatch(ReturningGroupFunction returningGroupFunction)
+        {
+            try
+            {
+                return await returningGroupFunction();
+            }
+            catch (NullGroupException nullGroupException)
+            {
+               throw CreateAndLogValidationException(nullGroupException);
+            }
+
+        }
+
+        private GroupValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var groupValidationException = new GroupValidationException(exception);
+            this.loggingBroker.LogError(groupValidationException);
+
+            throw groupValidationException;
+        }
+    }
+}
