87cd71ac8a8379ad9eea30ebaa1fd4dc41f87031
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:55:41 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

---
+++ DemoCenter/Services/Foundations/Subjects/SubjectService.Validations.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.Xml;
+using DemoCenter.Models.Subjects;
+using DemoCenter.Models.Subjects.Exceptions;
+
+namespace DemoCenter.Services.Foundations.Subjects
+{
+    public partial class SubjectService
+    {
+        private static void ValidateSubjectNotNull(Subject subject)
+        {
+            if(subject is null)
+            {
+                throw new NullSubjectException();
+            }
+        }
+    }
+}
--- DemoCenter/Services/Foundations/subjects/SubjectService.cs
+++ DemoCenter/Services/Foundations/subjects/SubjectService.cs
@@ -8,7 +8,7 @@ using DemoCenter.Models.Subjects;
 
 namespace DemoCenter.Services.Foundations.Subjects
 {
-    public class SubjectService : ISubjectService
+    public partial class SubjectService : ISubjectService
     {
         private readonly IStorageBroker storageBroker;
         private readonly IDateTimeBroker dateTimeBroker;
@@ -24,13 +24,18 @@ namespace DemoCenter.Services.Foundations.Subjects
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Subject> AddSubjectAsync(Subject subject) =>
-            await this.storageBroker.InsertSubjectAsync(subject);
+        public ValueTask<Subject> AddSubjectAsync(Subject subject) =>
+        TryCatch(async () =>
+        {
+            ValidateSubjectNotNull(subject);
+            return await this.storageBroker.InsertSubjectAsync(subject);
+
+        });
 
         public IQueryable<Subject> RetrieveAllSubjects() =>
             this.storageBroker.SelectAllSubjects();
 
-        public async ValueTask<Subject> RetrieveSubjectByIdAsync(Guid subjectId)=>
+        public async ValueTask<Subject> RetrieveSubjectByIdAsync(Guid subjectId) =>
            await this.storageBroker.SelectSubjectByIdAsync(subjectId);
         public async ValueTask<Subject> RemoveSubjectByIdAsync(Guid subjectId)
         {
