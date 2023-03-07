04ed532ead253469fa703d61a7e14fe8ce56fb07
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:09:54 +0500

    FOUDATIONS : Logic RemoveByIdTeacher Test

---
+++ DemoCenter.Test.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.RemoveById.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Teachers;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Teachers
+{
+    public partial class TeacherServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveSubjectByIdAsync()
+        {
+            //given
+            Guid randomId= Guid.NewGuid();
+            Guid inputTeacherId = randomId;
+            Teacher randomTeacher=CreateRandomTeacher();
+            Teacher storageTeacher = randomTeacher;
+            Teacher expectedInputTeacher = storageTeacher;
+            Teacher deletedTeacher = expectedInputTeacher;
+            Teacher expectedTeacher = deletedTeacher.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectTeacherByIdAsync(inputTeacherId)).ReturnsAsync(storageTeacher);
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.DeleteTeacherAsync(expectedInputTeacher)).ReturnsAsync(deletedTeacher);
+
+            //when
+            Teacher actualTeacher =await
+                this.teacherService.RemoveTeacherByIdAsync(inputTeacherId);
+
+            //then
+            actualTeacher.Should().BeEquivalentTo(expectedTeacher);
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.SelectTeacherByIdAsync(inputTeacherId), Times.Once());   
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.DeleteTeacherAsync(expectedInputTeacher), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();    
+        }
+    }
+}
