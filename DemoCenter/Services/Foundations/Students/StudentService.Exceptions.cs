2efef7df643735d8523cd3ce89daf7a88370f49b
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:06:04 +0500

    FOUDATIONS : Add Exceptions And Validations Classes

---
+++ DemoCenter/Services/Foundations/Students/StudentService.Exceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+using Xeptions;
+
+namespace DemoCenter.Services.Foundations.Students
+{
+    public partial class StudentService
+    {
+        private delegate ValueTask<Student> ReturningStudentFunction();
+
+        private async ValueTask<Student> TryCatch(ReturningStudentFunction returningStudentFunction)
+        {
+            try
+            {
+                return await returningStudentFunction();
+            }
+            catch (NullStudentException nullStudentException)
+            {
+
+                throw CreateAndLogValidationException(nullStudentException);
+            }
+        }
+
+        private StudentValidationException CreateAndLogValidationException(Xeption exception)
+        {
+           var studentValidationException=new StudentValidationException(exception);
+            this.loggingBroker.LogError(studentValidationException);
+
+            throw studentValidationException;
+        }
+    }
+}
---
+++ DemoCenter/Services/Foundations/Students/StudentService.Validations.cs
@@ -0,0 +1,16 @@
+﻿using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+
+namespace DemoCenter.Services.Foundations.Students
+{
+    public partial class StudentService
+    {
+        private static void ValidationStudentNotNull(Student student)
+        {
+            if (student == null)
+            {
+                throw new NullStudentException();
+            }
+        }
+    }
+}
