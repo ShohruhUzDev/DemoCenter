0ac6e136e923bea4ccf3ea4b681d3a557cffdb6a
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:04:27 +0500

    CODE RUB : Add ValidationsException And NullException

---
+++ DemoCenter/Models/Groups/Exceptions/GroupValidationException.cs
@@ -0,0 +1,13 @@
+﻿using Xeptions;
+
+namespace DemoCenter.Models.Groups.Exceptions
+{
+    public class GroupValidationException : Xeption
+    {
+        public GroupValidationException(Xeption innerException):
+            base(message: "Group validation error occured, fix the error and try again.", innerException)
+        {
+            
+        }
+    }
+}
