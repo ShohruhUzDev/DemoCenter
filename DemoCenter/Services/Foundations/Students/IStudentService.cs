203598398bca3a1befa4fc64fac149042e3b8d18
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:06:56 +0500

    CODE RUB : Fixed All Methods

--- DemoCenter/Services/Foundations/Students/IStudentService.cs
+++ DemoCenter/Services/Foundations/Students/IStudentService.cs
@@ -10,5 +10,7 @@ namespace DemoCenter.Services.Foundations.Students
         ValueTask<Student> AddStudentAsync(Student student);
         IQueryable<Student> RetrieveAllStudents();
         ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId);
+        ValueTask<Student> ModifyStudentAsync(Student student);
+        ValueTask<Student> RemoveStudentByIdAsync(Guid studentId);
     }
 }
