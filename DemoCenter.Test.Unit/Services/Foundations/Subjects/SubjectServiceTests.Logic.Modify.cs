33e10d9fbe8263b805645055b7db3e4f7d338256
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:08:30 +0500

    FOUDATIONS : Logic ModifySubject Test

---
+++ DemoCenter.Test.Unit/Services/Foundations/Subjects/SubjectServiceTests.Logic.Modify.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Subjects
+{
+    public partial class SubjectServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifySubjectAsync()
+        {
+            //given
+            //when
+            //then
+        }
+    }
+}
