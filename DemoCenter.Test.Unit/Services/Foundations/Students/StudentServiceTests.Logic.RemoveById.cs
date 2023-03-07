a5e6250cb802770a4d40cc15bd1df0cc5a737adf
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:09:29 +0500

    FOUDATIONS : Logic RemoveByIdStudent Test

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Logic.RemoveById.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveStudentByIdAsync()
+        {
+            //given
+            Guid randomStudentId= Guid.NewGuid();
+            Guid inputStudentId = randomStudentId;
+            Student randomStudent=CreateRandomStudent();
+            Student storageStudent = randomStudent;
+            Student expectedInputStudent = storageStudent;
+            Student deletedStudent = expectedInputStudent;
+            Student expectedStudent = deletedStudent.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectStudentByIdAsync(inputStudentId)).ReturnsAsync(storageStudent);
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.DeleteStudentAsync(expectedInputStudent)).ReturnsAsync(deletedStudent);
+
+            //when
+            Student actualStudent=await this.studentService.RemoveStudentByIdAsync(inputStudentId);
+
+            //then
+            actualStudent.Should().BeEquivalentTo(expectedStudent);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.SelectStudentByIdAsync(inputStudentId), Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.DeleteStudentAsync(expectedInputStudent), Times.Once()); 
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();   
+            this.loggingBrokerMock.VerifyNoOtherCalls();    
+        }
+    }
+}
