774d9d15856c766c7b33d0dad8fe8826f1596745
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-07 17:08:18 +0500

    FOUDATIONS : Logic ModifyStudent Test

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Modify.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyStudentAsync()
+        {
+            //given
+            DateTimeOffset randomDate=GetRandomDateTimeOffset();
+            Student randomStudent = CreateRandomModifyStudent(randomDate);
+            Student inputStudent = randomStudent;
+            Student storageStudent = inputStudent.DeepClone();
+            storageStudent.UpdatedDate = randomStudent.CreatedDate;
+            Student updatedStudent = inputStudent;
+            Student expectedStudent = updatedStudent.DeepClone();
+            Guid studentId=inputStudent.Id;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+            broker.GetCurrenDateTime()).Returns(randomDate);
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectStudentByIdAsync(studentId)).ReturnsAsync(storageStudent);
+
+            this.storageBrokerMock.Setup(broker=>
+            broker.UpdateStudentAsync(inputStudent)).ReturnsAsync(updatedStudent);
+
+            //when
+            Student actualStudent =await
+                this.studentService.ModifyStudentAsync(inputStudent);
+
+            //then
+            actualStudent.Should().BeEquivalentTo(expectedStudent);
+
+            this.dateTimeBrokerMock.Verify(broker=>
+            broker.GetCurrenDateTime(), Times.Never);
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.SelectStudentByIdAsync(studentId), Times.Once);  
+
+            this.storageBrokerMock.Verify(broker=>
+            broker.UpdateStudentAsync(inputStudent), Times.Once);   
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
