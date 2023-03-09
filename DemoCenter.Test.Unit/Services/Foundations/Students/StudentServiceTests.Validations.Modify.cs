8bfc0185ac6756ebef27595fb0c282d581eaf496
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-09 22:45:18 +0500

    ShouldThrowValidationExceptionOnModifyIfStudentIsNullAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Modify.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfStudentIsNullAndLogItAsync()
+        {
+            //given
+            Student nullStudent = null;
+            var nullStudentException = new NullStudentException();
+
+            var expectedStudentValidationException=new StudentValidationException(nullStudentException); 
+
+            //when
+            ValueTask<Student> onModifyStudentTask=this.studentService.ModifyStudentAsync(nullStudent);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(onModifyStudentTask.AsTask);
+
+            //then
+            actualStudentValidationException.Should().BeEquivalentTo(expectedStudentValidationException);
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.UpdateStudentAsync(It.IsAny<Student>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
