7dc3997415688b491c53a2a645280f905c49a9b1
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-09 13:00:10 +0500

    ShouldThrowValidationExceptionOnAddIfInputInvalidAndLogItAsync ->FAIL

---
+++ DemoCenter/Models/Teachers/Exceptions/InvalidTeacherException.cs
@@ -0,0 +1,13 @@
+﻿using Xeptions;
+
+namespace DemoCenter.Models.Teachers.Exceptions
+{
+    public class InvalidTeacherException : Xeption
+    {
+        public InvalidTeacherException() 
+            :base(message:"Teacher is invalid.")
+        {
+            
+        }
+    }
+}
--- DemoCenter/Services/Foundations/Teachers/TeacherService.Validations.cs
+++ DemoCenter/Services/Foundations/Teachers/TeacherService.Validations.cs
@@ -1,10 +1,43 @@
-﻿using DemoCenter.Models.Teachers;
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using DemoCenter.Models.Teachers;
 using DemoCenter.Models.Teachers.Exceptions;
 
 namespace DemoCenter.Services.Foundations.Teachers
 {
     public partial class TeacherService
     {
+        public void ValidationTeacherOnAdd(Teacher teacher)
+        {
+            ValidationTeacherNotNull(teacher);
+
+            Validate(
+                (Rule: IsInvalid(teacher.Id), Parameter: nameof(Teacher.Id)),
+                (Rule: IsInvalid(teacher.FirstName), Parameter: nameof(Teacher.FirstName)),
+                (Rule: IsInvalid(teacher.LastName), Parameter: nameof(Teacher.LastName)),
+                (Rule: IsInvalid(teacher.CreatedDate), Parameter: nameof(Teacher.CreatedDate)),
+                (Rule: IsInvalid(teacher.UpdatedDate), Parameter: nameof(Teacher.UpdatedDate)));
+
+        }
+        public static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == default,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Value si required"
+        };
         private static void ValidationTeacherNotNull(Teacher teacher)
         {
             if (teacher is null)
@@ -12,6 +45,22 @@ namespace DemoCenter.Services.Foundations.Teachers
                 throw new NullTeacherException();
 
             }
+        }
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidTeacherExceptioin = new InvalidTeacherException();
+
+            foreach((dynamic rule, string parameter) in validations)
+
+            {
+                if(rule.Condition)
+                {
+                    invalidTeacherExceptioin.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+            invalidTeacherExceptioin.ThrowIfContainsErrors();
         }
     }
 }
