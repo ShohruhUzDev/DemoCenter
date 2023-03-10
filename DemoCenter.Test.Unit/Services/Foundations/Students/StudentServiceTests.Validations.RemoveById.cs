806955c777ef63a838ee1e06670bbc0bc5632660
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-10 14:40:18 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidStudentId = Guid.Empty;
+            var invalidStudentException = new InvalidStudentException();
+
+            invalidStudentException.AddData(
+                key: nameof(Student.Id),
+                values: "Id is required");
+            
+            var expectedGroupValidationException=new StudentValidationException(invalidStudentException);
+            
+            //when
+            ValueTask<Student> onRemoveStudentTask=this.studentService.RemoveStudentByIdAsync(invalidStudentId);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(onRemoveStudentTask.AsTask);
+          
+            //then
+            actualStudentValidationException.Should().BeEquivalentTo(expectedGroupValidationException);
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(expectedGroupValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteStudentAsync(It.IsAny<Student>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();   
+        }
+    }
+}
