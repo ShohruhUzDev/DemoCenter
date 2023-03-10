1456f02e5e55a541b2023215fb32b87c164bdd0a
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-10 15:37:41 +0500

    ShouldThrowValidationExceptionOnRetrieveIfIdIsInvalidAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Groups/GroupServiceTests.Validatons.RetrieveById.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using DemoCenter.Models.Groups.Exceptions;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Groups
+{
+    public partial class GroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidGroupId = Guid.Empty;
+            var invalidGroupException = new InvalidGroupException();
+
+            invalidGroupException.AddData(
+                key: nameof(Group.Id),
+                values: "Id is required");
+
+            var expectedGroupValidationException=
+                new GroupValidationException(invalidGroupException);   
+
+            //when
+            ValueTask<Group> onRetrieveGroupTask=this.groupService.RetrieveGroupByIdAsync(invalidGroupId);
+
+            GroupValidationException actualGroupValidationException =
+                await Assert.ThrowsAsync<GroupValidationException>(onRetrieveGroupTask.AsTask);
+
+            //then
+            actualGroupValidationException.Should().BeEquivalentTo(expectedGroupValidationException);   
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptonAs(expectedGroupValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.SelectGroupByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
