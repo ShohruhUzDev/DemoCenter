ffee9774ffe29947ac1be37e0a7e59f0b0e1b5bf
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-11 12:04:01 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfTeacherNotFoundAndLogItAsync ->FAIL

--- DemoCenter.Test.Unit/Services/Foundations/Teachers/TeacherServiceTests.Validations.RetrieveById.cs
+++ DemoCenter.Test.Unit/Services/Foundations/Teachers/TeacherServiceTests.Validations.RetrieveById.cs
@@ -45,5 +45,40 @@ namespace DemoCenter.Test.Unit.Services.Foundations.Teachers
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfTeacherNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someTeacherId= Guid.NewGuid(); 
+            Teacher noTeacher= null;
+            var noutFoundTeacherException=new NotFoundTeacherException(someTeacherId);
+
+            var expectedTeacherValidationException = 
+                new TeacherValidationException(noutFoundTeacherException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTeacherByIdAsync(It.IsAny<Guid>())).ReturnsAsync(noTeacher);
+
+            //when
+            ValueTask<Teacher> onRetrieveTeacherTask = this.teacherService.RetrieveTeacherByIdAsync(someTeacherId);
+
+            TeacherValidationException actualTeacherValidationException =
+                await Assert.ThrowsAsync<TeacherValidationException>(onRetrieveTeacherTask.AsTask);
+
+            //then
+            actualTeacherValidationException.Should()
+                .BeEquivalentTo(expectedTeacherValidationException);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.SelectTeacherByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(expectedTeacherValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ DemoCenter/Models/Teachers/Exceptions/NotFoundTeacherException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace DemoCenter.Models.Teachers.Exceptions
+{
+    public class NotFoundTeacherException : Xeption
+    {
+        public NotFoundTeacherException(Guid teacherId):
+            base(message:$"Couldn't found teacher with id: {teacherId}")
+        {            
+        }
+    }
+}
