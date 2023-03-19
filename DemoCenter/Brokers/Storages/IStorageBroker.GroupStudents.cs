a6648b7839397bf24a325101eada3bd86d4579fd
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-19 18:25:17 +0500

    BROKERS : Add New Model

---
+++ DemoCenter/Brokers/Storages/IStorageBroker.GroupStudents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoCenter.Models.GroupStudents;
+
+namespace DemoCenter.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<GroupStudent> InsertGroupStudentAsync(GroupStudent groupPost);
+        IQueryable<GroupStudent> SelectAllGroupStudents();
+        ValueTask<GroupStudent> SelectGroupStudentByIdAsync(Guid groupId, Guid studentId);
+        ValueTask<GroupStudent> UpdateGroupStudentAsync(GroupStudent groupStudent);
+        ValueTask<GroupStudent> DeleteGroupStudentAsync(GroupStudent groupStudent);
+    }
+}
