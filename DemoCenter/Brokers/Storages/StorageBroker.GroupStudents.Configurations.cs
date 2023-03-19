cb274b48648f775753bfe036314d28d7ac7f3489
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-19 18:28:46 +0500

    BROKERS: Add New Class

---
+++ DemoCenter/Brokers/Storages/StorageBroker.GroupStudents.Configurations.cs
@@ -0,0 +1,27 @@
+﻿using DemoCenter.Models.GroupStudents;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoCenter.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private static void AddGroupStudentConfigurations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GroupStudent>()
+                .HasKey(groupStudent =>
+                new { groupStudent.GroupId, groupStudent.StudentId });
+
+            modelBuilder.Entity<GroupStudent>()
+                .HasOne(groupStudent=>groupStudent.Student)
+                .WithMany(student=>student.GroupStudents)
+                .HasForeignKey(groupStudent => groupStudent.GroupId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<GroupStudent>()
+                .HasOne(groupStudent=>groupStudent.Group)
+                .WithMany(group=>group.GroupStudents)
+                .HasForeignKey(groupStudents=>groupStudents.GroupId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
