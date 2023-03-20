7022598f6f467cf9aa50d11fa8a1edd7d018dc45
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-20 15:00:43 +0500

    ShouldAddUserAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/users/UserServiceTests.Logic.Add.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Users;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddUserAsync()
+        {
+            //given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            User randomUser = CreateRandomUser(randomDateTime);
+            User inputUser = randomUser;
+            User persistedUser = inputUser;
+            User expectedUser = persistedUser.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker=>
+                broker.GetCurrenDateTime()).Returns(randomDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserAsync(inputUser))
+                .ReturnsAsync(persistedUser);
+
+            //when
+            User actualUser=await this.userService.AddUserAsync(inputUser);
+
+            //then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.dateTimeBrokerMock.Verify(broker=>
+                broker.GetCurrenDateTime(), Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                    broker.InsertUserAsync(inputUser), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- DemoCenter.Test.Unit/Services/Foundations/users/UserServiceTests.cs
+++ DemoCenter.Test.Unit/Services/Foundations/users/UserServiceTests.cs
@@ -1,8 +1,11 @@
-﻿using DemoCenter.Brokers.DateTimes;
+﻿using System;
+using DemoCenter.Brokers.DateTimes;
 using DemoCenter.Brokers.Loggings;
 using DemoCenter.Brokers.Storages;
+using DemoCenter.Models.Users;
 using DemoCenter.Services.Foundations.Users;
 using Moq;
+using Tynamix.ObjectFiller;
 
 namespace DemoCenter.Test.Unit.Services.Foundations.Users
 {
@@ -17,11 +20,30 @@ namespace DemoCenter.Test.Unit.Services.Foundations.Users
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
             this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();  
-            this.loggingBrokerMock = new Mock<ILoggingBroker>();    
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
-            this.userService=new UserService(
-                stora)
+            this.userService = new UserService(
+                storageBroker: storageBrokerMock.Object,
+                dateTimeBroker: dateTimeBrokerMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
 
         }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static User CreateRandomUser(DateTimeOffset date) =>
+            CreateUserFiller(GetRandomDateTime()).Create();
+
+        private static Filler<User> CreateUserFiller(DateTimeOffset date)
+        {
+            var filler= new Filler<User>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+
     }
 }
