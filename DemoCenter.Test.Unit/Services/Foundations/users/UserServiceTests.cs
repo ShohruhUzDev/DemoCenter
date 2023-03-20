f090b5d32990efaa3bbfd6b3c100fd0a0333bc30
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-20 14:43:25 +0500

    CODE RUB : Implement User Methods

---
+++ DemoCenter.Test.Unit/Services/Foundations/users/UserServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using DemoCenter.Brokers.DateTimes;
+using DemoCenter.Brokers.Loggings;
+using DemoCenter.Brokers.Storages;
+using DemoCenter.Services.Foundations.Users;
+using Moq;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IUserService userService;
+
+        public UserServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();  
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();    
+
+            this.userService=new UserService(
+                stora)
+
+        }
+    }
+}
