8538fcd40cb8fdc38c456d8a21576d99ad5a7c5d
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-22 15:23:41 +0500

    ShouldThrowCriticalDepedencyExceptionOnAddIfSqlErrorOccursAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Groups/GroupServiceTests.Exceptions.Add.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using DemoCenter.Models.Groups.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Groups
+{
+    public partial class GroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDepedencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Group someGroup = CreateRandomGroup();
+            SqlException sqlException = CreateSqlException();
+            var failedGroupStorageException = new FailedGroupStorageException(sqlException);
+
+            var expectedDependencyException =
+                new GroupDependencyException(failedGroupStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrenDateTime()).Throws(sqlException);
+
+            //when
+            ValueTask<Group> addGroupTask=this.groupService.AddGroupAsync(someGroup);
+
+            GroupDependencyException actualGroupDependencyException =
+                await Assert.ThrowsAsync<GroupDependencyException>(addGroupTask.AsTask);
+
+            //then
+            actualGroupDependencyException.Should().BeEquivalentTo(expectedDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrenDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptonAs(
+                    expectedDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.InsertGroupAsync(It.IsAny<Group>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DemoCenter/Models/Groups/Exceptions/FailedGroupStorageException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace DemoCenter.Models.Groups.Exceptions
+{
+    public class FailedGroupStorageException : Xeption
+    {
+        public FailedGroupStorageException(Exception innerException) 
+            : base(message : "Failed group storage error occured, contact support.", innerException)
+        {}
+    }
+}
