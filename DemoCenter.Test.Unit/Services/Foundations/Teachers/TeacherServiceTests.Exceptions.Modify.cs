b1cbd2afc2a553a10a11aab85c447ba564c03ddb
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-23 03:30:25 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Teachers/TeacherServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoCenter.Models.Teachers;
+using DemoCenter.Models.Teachers.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Teachers
+{
+    public partial class TeacherServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset someDateTime = GetRandomDateTimeOffset();
+            Teacher randomTeacher = CreateRandomTeacher(someDateTime);
+            Teacher someTeacher = randomTeacher;
+            Guid TeacherId = someTeacher.Id;
+            SqlException sqlException = CreateSqlException();
+
+            var failedTeacherStorageException =
+                new FailedTeacherStorageException(sqlException);
+
+            var expectedTeacherDependencyException =
+                new TeacherDependencyException(failedTeacherStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Throws(sqlException);
+
+            // when
+            ValueTask<Teacher> modifyTeacherTask =
+                this.teacherService.ModifyTeacherAsync(someTeacher);
+
+            TeacherDependencyException actualTeacherDependencyException =
+                await Assert.ThrowsAsync<TeacherDependencyException>(
+                     modifyTeacherTask.AsTask);
+
+            // then
+            actualTeacherDependencyException.Should().BeEquivalentTo(
+                expectedTeacherDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTeacherDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTeacherByIdAsync(TeacherId), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTeacherAsync(someTeacher), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DemoCenter/Controllers/TeachersController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using DemoCenter.Models.Teachers;
+using DemoCenter.Models.Teachers.Exceptions;
+using DemoCenter.Services.Foundations.Teachers;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace DemoCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeachersController : RESTFulController
+    {
+        private readonly ITeacherService teacherService;
+
+        public TeachersController(ITeacherService teacherService) =>
+            this.teacherService = teacherService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Teacher>> PostTeacherAsync(Teacher teacher)
+        {
+            try
+            {
+                return Created(await this.teacherService.AddTeacherAsync(teacher));
+            }
+          
+            catch (TeacherValidationException TeacherValidationExpection)
+            {
+                return BadRequest(TeacherValidationExpection.InnerException);
+            }
+            catch (TeacherDependencyValidationException TeacherDependencyValidationException)
+                when (TeacherDependencyValidationException.InnerException is AlreadyExistTeacherException)
+            {
+                return Conflict(TeacherDependencyValidationException.InnerException);
+            }
+            catch (TeacherDependencyValidationException TeacherDependencyValidationException)
+            {
+                return BadRequest(TeacherDependencyValidationException.InnerException);
+            }
+            catch (TeacherDependencyException TeacherDependencyException)
+            {
+                return InternalServerError(TeacherDependencyException.InnerException);
+            }
+            catch (TeacherServiceException TeacherServiceException)
+            {
+                return InternalServerError(TeacherServiceException.InnerException);
+            }
+        }
+        
+    }
+}
