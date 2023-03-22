0b1016d63f8878415b49bb7d5e4bb4c61015771b
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-22 17:49:46 +0500

    CODE RUB : Fix Mistakes

--- DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Add.cs
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Add.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DemoCenter.Models.Students;
 using FluentAssertions;
 using Force.DeepCloner;
@@ -13,10 +14,14 @@ namespace DemoCenter.Test.Unit.Services.Foundations.Students
         public async Task ShouldAddStudentAsync()
         {
             //given
-            Student randomStudent = CreateRandomStudent();
+            DateTimeOffset randomDateTime = GetRandomDateTimeOffset();
+            Student randomStudent = CreateRandomStudent(randomDateTime);
             Student inputStudent = randomStudent;
             Student persistedStudent = inputStudent;
             Student expectedStuden = persistedStudent.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker=>
+                broker.GetCurrenDateTime()).Returns(randomDateTime);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertStudentAsync(inputStudent)).ReturnsAsync(persistedStudent);
@@ -28,10 +33,14 @@ namespace DemoCenter.Test.Unit.Services.Foundations.Students
             //then
             actualStudent.Should().BeEquivalentTo(expectedStuden);
 
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrenDateTime(), Times.Once);
+
             this.storageBrokerMock.Verify(broker =>
                  broker.InsertStudentAsync(inputStudent), Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
