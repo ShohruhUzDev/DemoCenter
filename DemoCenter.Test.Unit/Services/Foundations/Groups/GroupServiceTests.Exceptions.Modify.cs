b4b5fb6b3e6f19fbfaa8d152a39b2e8f1ebd6263
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-23 01:28:24 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync ->PASS

---
+++ DemoCenter.Test.Unit/Services/Foundations/Groups/GroupServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoCenter.Models.Groups;
+using DemoCenter.Models.Groups.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Groups
+{
+    public partial class GroupServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset someDateTime = GetRandomDateTimeOffset();
+            Group randomGroup = CreateRandomGroup(someDateTime);
+            Group someGroup = randomGroup;
+            Guid GroupId = someGroup.Id;
+            SqlException sqlException = CreateSqlException();
+
+            var failedGroupStorageException =
+                new FailedGroupStorageException(sqlException);
+
+            var expectedGroupDependencyException =
+                new GroupDependencyException(failedGroupStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrenDateTime()).Throws(sqlException);
+
+            // when
+            ValueTask<Group> modifyGroupTask =
+                this.groupService.ModifyGroupAsync(someGroup);
+
+            GroupDependencyException actualGroupDependencyException =
+                await Assert.ThrowsAsync<GroupDependencyException>(
+                     modifyGroupTask.AsTask);
+
+            // then
+            actualGroupDependencyException.Should().BeEquivalentTo(
+                expectedGroupDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrenDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptonAs(
+                    expectedGroupDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGroupByIdAsync(GroupId), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGroupAsync(someGroup), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
