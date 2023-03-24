5fcd6ec590d09c1da75e70812305d648364957c7
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-25 02:22:26 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync ->FAIL

--- DemoCenter.Test.Unit/Services/Foundations/Subjects/SubjectServiceTests.Exceptions.Modify.cs
+++ DemoCenter.Test.Unit/Services/Foundations/Subjects/SubjectServiceTests.Exceptions.Modify.cs
@@ -4,6 +4,7 @@ using DemoCenter.Models.Subjects;
 using DemoCenter.Models.Subjects.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -58,6 +59,56 @@ namespace DemoCenter.Test.Unit.Services.Foundations.Subjects
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
 
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            //given
+            int minutesInPas = GetRandomNegativeNumber();
+            DateTimeOffset randomDate = GetRandomDateTime();
+            Subject randomSubject = CreateRandomSubject(randomDate);
+            Subject someSubject = randomSubject;
+            Guid someId = someSubject.Id;
+            someSubject.CreatedDate = randomDate.AddMinutes(minutesInPas);
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedSubjectStorageException=
+                new FailedSubjectStorageException(databaseUpdateException);
+
+            var expectedSubjectDependencyException =
+                new SubjectDependencyException(failedSubjectStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSubjectByIdAsync(someId)).ThrowsAsync(databaseUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker=>
+                broker.GetCurrentDateTime()).Returns(randomDate);
+
+            //when
+            ValueTask<Subject> modifySubjectTask =
+                this.subjectService.ModifySubjectAsync(someSubject);
+
+            SubjectDependencyException actualSubjectDependencyException =
+                await Assert.ThrowsAsync<SubjectDependencyException>(modifySubjectTask.AsTask);
+
+            //then
+            actualSubjectDependencyException.Should()
+                .BeEquivalentTo(expectedSubjectDependencyException);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.SelectSubjectByIdAsync(someId), Times.Once());   
+
+            this.dateTimeBrokerMock.Verify(broker=>
+                broker.GetCurrentDateTime(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubjectDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
