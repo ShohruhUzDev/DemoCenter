d25e79968c07a3be42ad96a06f45d72f74d4def8
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-26 01:18:55 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync ->PASS

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedStudentStorageException =
+                new FailedStudentStorageException(sqlException);
+
+            var expectedStudentDependencyException =
+                new StudentDependencyException(failedStudentStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Student> retrieveStudentByIdTask =
+                this.StudentService.RetrieveStudentByIdAsync(someId);
+
+            StudentDependencyException actaulStudentDependencyException =
+                await Assert.ThrowsAsync<StudentDependencyException>(
+                    retrieveStudentByIdTask.AsTask);
+
+            // then
+            actaulStudentDependencyException.Should().BeEquivalentTo(
+                expectedStudentDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedStudentDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
