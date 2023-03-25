587cbe8143ef6e23178a885ceaf161605af5b538
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-26 01:10:51 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync ->PASS

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+using FluentAssertions;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset someDateTime = GetRandomDateTime();
+            Student randomStudent = CreateRandomStudent(someDateTime);
+            Student someStudent = randomStudent;
+            Guid StudentId = someStudent.Id;
+            SqlException sqlException = CreateSqlException();
+
+            var failedStudentStorageException =
+                new FailedStudentStorageException(sqlException);
+
+            var expectedStudentDependencyException =
+                new StudentDependencyException(failedStudentStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Throws(sqlException);
+
+            // when
+            ValueTask<Student> modifyStudentTask =
+                this.studentService.ModifyStudentAsync(someStudent);
+
+            StudentDependencyException actualStudentDependencyException =
+                await Assert.ThrowsAsync<StudentDependencyException>(
+                     modifyStudentTask.AsTask);
+
+            // then
+            actualStudentDependencyException.Should().BeEquivalentTo(
+                expectedStudentDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedStudentDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentByIdAsync(StudentId), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateStudentAsync(someStudent), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
