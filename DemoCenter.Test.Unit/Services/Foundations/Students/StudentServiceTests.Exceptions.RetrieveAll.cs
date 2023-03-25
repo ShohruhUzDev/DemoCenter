9802d6c8cce2fb88dfd1b9332fe234d244158d12
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-26 01:26:18 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Students/StudentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,49 @@
+﻿using DemoCenter.Models.Students.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStudentStorageException =
+                new FailedStudentStorageException(sqlException);
+
+            var expectedStudentDependencyException =
+                new StudentDependencyException(failedStudentStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStudents()).Throws(sqlException);
+
+            // when
+            Action retrieveAllStudentsAction = () =>
+                this.studentService.RetrieveAllStudents();
+
+            StudentDependencyException actualStudentDependencyException =
+                Assert.Throws<StudentDependencyException>(retrieveAllStudentsAction);
+
+            // then
+            actualStudentDependencyException.Should().BeEquivalentTo(expectedStudentDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStudents(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedStudentDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
