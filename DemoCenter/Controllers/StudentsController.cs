aafd0a2d20a6da0781e31fd804121eb9c650c0a5
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-27 00:24:00 +0500

    CONTROLLERS : Post Student

---
+++ DemoCenter/Controllers/StudentsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using DemoCenter.Models.Students;
+using DemoCenter.Models.Students.Exceptions;
+using DemoCenter.Services.Foundations.Students;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace DemoCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : RESTFulController
+    {
+        private readonly IStudentService studentService;
+
+        public StudentsController(IStudentService studentService) =>
+            this.studentService = studentService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Student>> PostStudentAsync(Student student)
+        {
+            try
+            {
+                Student createdStudent = await this.studentService.AddStudentAsync(student);
+
+                return Created(createdStudent);
+            }
+            catch (StudentValidationException studentValidationException)
+            {
+                return BadRequest(studentValidationException.InnerException);
+            }
+            catch (StudentDependencyValidationException studentValidationException)
+                when (studentValidationException.InnerException is AlreadyExistsStudentException)
+            {
+                return Conflict(studentValidationException.InnerException);
+            }
+            catch (StudentDependencyValidationException studentDependencyValidationException)
+            {
+                return BadRequest(studentDependencyValidationException.InnerException);
+            }
+            catch (StudentDependencyException studentDependencyException)
+            {
+                return InternalServerError(studentDependencyException.InnerException);
+            }
+            catch (StudentServiceException studentServiceException)
+            {
+                return InternalServerError(studentServiceException.InnerException);
+            }
+        }
+
+    }
+}
