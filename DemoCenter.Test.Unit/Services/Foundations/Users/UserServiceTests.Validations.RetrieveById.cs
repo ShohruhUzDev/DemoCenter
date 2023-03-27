0c815512cf9c2facdbfe0c381a2e37217daefe41
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-28 00:14:35 +0500

    ShouldThrowVlidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync ->FAIL

---
+++ DemoCenter.Test.Unit/Services/Foundations/Users/UserServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,51 @@
+﻿using DemoCenter.Models.Users;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Tarteeb.Api.Models.Foundations.Users.Exceptions;
+using Xunit;
+using FluentAssertions;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowVlidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invlidUserId = Guid.Empty;
+            var invalidUserException = new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            var expectedUserValidationException =
+                new UserValidationException(invalidUserException);
+
+            // when
+            ValueTask<User> retrieveUserByIdTask =
+                this.userService.RetrieveUserByIdAsync(invlidUserId);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(retrieveUserByIdTask.AsTask);
+
+            // then
+            actualUserValidationException.Should()
+                .BeEquivalentTo(expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
