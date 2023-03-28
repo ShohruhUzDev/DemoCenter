934b8b153ebb214c129895ec35f43306ec981b66
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-28 18:15:04 +0500

    CONTROLLERS : Delete Group

--- DemoCenter/Controllers/GroupsController.cs
+++ DemoCenter/Controllers/GroupsController.cs
@@ -67,9 +67,9 @@ namespace DemoCenter.Controllers
             {
                 return InternalServerError(groupDepdencyException.InnerException);
             }
-            catch(GroupServiceException groupServiceException)
+            catch (GroupServiceException groupServiceException)
             {
-                return InternalServerError(groupServiceException.InnerException);   
+                return InternalServerError(groupServiceException.InnerException);
             }
         }
 
@@ -100,5 +100,83 @@ namespace DemoCenter.Controllers
                 return InternalServerError(groupServiceException.InnerException);
             }
         }
+
+        [HttpPut]
+        public async ValueTask<ActionResult<Group>> PutGroupAsync(Group group)
+        {
+            try
+            {
+                Group updatedGroup = await this.groupService.ModifyGroupAsync(group);
+
+                return Ok(updatedGroup);
+            }
+            catch (GroupValidationException groupValidationException)
+                when (groupValidationException.InnerException is NotFoundGroupException)
+            {
+                return NotFound(groupValidationException.InnerException);
+            }
+            catch (GroupValidationException groupValidationException)
+            {
+                return BadRequest(groupValidationException.InnerException);
+            }
+            catch (GroupDependencyValidationException groupDepedencyValidationException)
+                when (groupDepedencyValidationException.InnerException is InvalidGroupReferenceException)
+            {
+                return FailedDependency(groupDepedencyValidationException.InnerException);
+            }
+            catch (GroupDependencyValidationException groupDepedencyValidationException)
+                when (groupDepedencyValidationException.InnerException is AlreadyExistGroupException)
+            {
+                return Conflict(groupDepedencyValidationException.InnerException);
+            }
+            catch (GroupDependencyException groupDepedencyException)
+            {
+                return InternalServerError(groupDepedencyException.InnerException);
+            }
+            catch (GroupServiceException groupServiceException)
+            {
+                return InternalServerError(groupServiceException.InnerException);
+            }
+
+
+        }
+
+        [HttpDelete("{groupId}")]
+        public async ValueTask<ActionResult<Group>> DeleteGroupAsync(Guid groupId)
+        {
+            try
+            {
+                Group deletedGroup = await this.groupService.RemoveGroupByIdAsync(groupId);
+
+                return Ok(deletedGroup);
+            }
+            catch (GroupValidationException groupValidationException)
+                when (groupValidationException.InnerException is NotFoundGroupException)
+            {
+                return NotFound(groupValidationException.InnerException);
+            }
+            catch (GroupValidationException groupValidationException)
+            {
+                return BadRequest(groupValidationException.InnerException);
+            }
+            catch (GroupDependencyValidationException groupDependencyValidationException)
+                when (groupDependencyValidationException.InnerException is LockedGroupException)
+            {
+                return Locked(groupDependencyValidationException.InnerException);
+            }
+            catch (GroupDependencyValidationException groupDepdencyValidationException)
+            {
+                return BadRequest(groupDepdencyValidationException.InnerException);
+            }
+            catch (GroupDependencyException groupDependencyException)
+            {
+                return InternalServerError(groupDependencyException.InnerException);
+            }
+            catch (GroupServiceException groupServiceException)
+            {
+                return InternalServerError(groupServiceException.InnerException);
+            }
+        }
+
     }
 }
