9d70e17387d9200f68f25e3825186e1c7288a31e
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-29 16:40:33 +0500

    ShouldThrowValidationExceptionOnModifyIfGroupStudentIsNullAndLogItAsync ->FAIL

--- DemoCenter.Test.Unit/Services/Foundations/GroupStudents/GroupStudentServiceTests.Validations.Modify.cs
+++ DemoCenter.Test.Unit/Services/Foundations/GroupStudents/GroupStudentServiceTests.Validations.Modify.cs
@@ -1,6 +1,42 @@
-﻿namespace DemoCenter.Test.Unit.Services.Foundations.GroupStudents
+﻿using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.GroupStudents
 {
     public partial class GroupStudentServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfGroupStudentIsNullAndLogItAsync()
+        {
+            //given
+            GroupStudent nullGroupStudent = null;
+            var nullGroupStudentException = new NullGroupStudentException();
+
+            GroupStudentValidationException expectedPostValidationException =
+                new GroupStudentValidationException(nullGroupStudentException);
+
+            //when
+            ValueTask<GroupStudent> modifyGroupStudentTask =
+                this.GroupStudentService.ModifyGroupStudentAsync(nullGroupStudent);
+
+            GroupStudentValidationException actualGroupStudentValidationException =
+                await Assert.ThrowsAsync<GroupStudentValidationException>(
+                    modifyGroupStudentTask.AsTask);
+
+            //then
+            actualGroupStudentValidationException.Should().BeEquivalentTo(
+                expectedPostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedPostValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGroupStudentAsync(nullGroupStudent), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
