ae576abe0506bb55a4da78c857290cddd403717d
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-29 18:34:48 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync ->FAIL

--- DemoCenter.Test.Unit/Services/Foundations/GroupStudents/GroupStudentServiceTests.Validations.RetrieveById.cs
+++ DemoCenter.Test.Unit/Services/Foundations/GroupStudents/GroupStudentServiceTests.Validations.RetrieveById.cs
@@ -1,6 +1,58 @@
-﻿namespace DemoCenter.Test.Unit.Services.Foundations.GroupStudents
+﻿using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace DemoCenter.Test.Unit.Services.Foundations.GroupStudents
 {
     public partial class GroupStudentServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidPostId = Guid.Empty;
+            Guid invalidProfileId = Guid.Empty;
+
+            var invalidGroupStudentException = new InvalidGroupStudentException();
+
+            invalidGroupStudentException.AddData(
+                key: nameof(GroupStudent.PostId),
+                values: "Id is required");
+
+            invalidGroupStudentException.AddData(
+                key: nameof(GroupStudent.ProfileId),
+                values: "Id is required");
+
+            var expectedGroupStudentValidationException = new
+                GroupStudentValidationException(invalidGroupStudentException);
+
+            // when
+            ValueTask<GroupStudent> retrieveGroupStudentByIdTask =
+                this.GroupStudentService.RetrieveGroupStudentByIdAsync(invalidPostId, invalidProfileId);
+
+            GroupStudentValidationException actualGroupStudentValidationException =
+                await Assert.ThrowsAsync<GroupStudentValidationException>(
+                    retrieveGroupStudentByIdTask.AsTask);
+
+            // then
+            actualGroupStudentValidationException.Should().BeEquivalentTo(
+                expectedGroupStudentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGroupStudentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGroupStudentByIdAsync(invalidPostId, invalidProfileId),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+
     }
 }
